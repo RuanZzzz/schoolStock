a3e26af69acbf171978147f6c963ef8d4a457258
RuanZzzz <734162396@qq.com>
2021-12-29 22:28:35 +0800

    完善sql语句

--- database/ruan.sql
+++ database/ruan.sql
@@ -2,9 +2,10 @@
 create table school_stock.goods
 (
     id            int auto_increment,
+    company       varchar(255) null comment '供货商',
     name          varchar(255) null,
-    specification varchar(255) null,
-    unit          varchar(255) null,
+    specification varchar(255) null comment '规格',
+    unit          varchar(255) null comment '单位',
     price         float        null,
     cate          varchar(255) null,
     remark        varchar(255) null,
@@ -17,18 +18,14 @@ create table school_stock.goods
 alter table school_stock.goods
     add primary key (id);
 
-
 -- 库存表
 create table school_stock.stock
 (
-    id                  int auto_increment,
-    goods_id            int           not null,
-    goods_name          varchar(255)  null,
-    goods_specification varchar(255)  null,
-    goods_unit          varchar(255)  null,
-    count               int default 0 null,
-    updated_at          varchar(255)  null,
-    created_at          varchar(255)  null,
+    id         int auto_increment,
+    goods_id   int           not null,
+    count      int default 0 null,
+    updated_at varchar(255)  null,
+    created_at varchar(255)  null,
     constraint stock_id_uindex
         unique (id)
 );
@@ -37,18 +34,18 @@ alter table school_stock.stock
     add primary key (id);
 
 
+
+
 -- 出库入库记录表
 create table school_stock.record
 (
     id                int auto_increment,
     goods_id          int          not null,
-    goods_name        varchar(255) not null,
     goods_count       int          null comment '入库/出库数量',
-    goods_unit        varchar(255) null,
     goods_price       float        null comment '物品单价',
     goods_total_price float        null comment '此次物品的总价',
-    goods_cate        varchar(255) null,
-    created_at        varchar(255) null,
+    record_time       varchar(255) null comment '入库/出库的时间，需要手动设置',
+    created_at        varchar(255) null comment '数据的创建时间',
     opera_type        varchar(255) not null comment '操作类型',
     record_name       varchar(255) not null comment '操作人名称',
     remark            varchar(255) null,
@@ -59,3 +56,7 @@ create table school_stock.record
 
 alter table school_stock.record
     add primary key (id);
+
+
+
+
