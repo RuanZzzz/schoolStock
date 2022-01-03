183735b3dc2a68b1ff52c0894861d6965423aa3e
RuanZzzz <734162396@qq.com>
2022-01-03 22:31:36 +0800

    暂时关闭物品信息的编辑功能

--- resources/views/admin/basics/goods/goodsElement.blade.php
+++ resources/views/admin/basics/goods/goodsElement.blade.php
@@ -9,7 +9,7 @@
         <td>{{get_last_time($goods->created_at)}}</td>
         <td style="text-align: center; ">
             {{--<a id="checkBtn" class="btn btn-primary btn-xs" data-id="" ><i class="fa fa-eye"></i>查看</a>--}}
-            <a id="editBtn" class="btn btn-primary btn-xs" data-id="{{$goods->id}}" ><i class="fa fa-edit"></i>修改</a>
+            {{--<a id="editBtn" class="btn btn-primary btn-xs" data-id="{{$goods->id}}" ><i class="fa fa-edit"></i>修改</a>--}}
             <a id="delBtn" class="btn btn-danger btn-xs" data-id="{{$goods->id}}" ><i class="fa fa-remove"></i>删除</a>
         </td>
     </tr>
